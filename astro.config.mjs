0e491f1212a34ef7d779d3c0ac4b02b2ad6cc600
ojolowoblue <ojolowoboluwatife1@gmail.com>
2025-01-16 01:39:12 +0100

    hot-update: make updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://engineer-blue.vercel.app',
+  site: 'https://ojolowo.com',
   integrations: [mdx(), sitemap(), tailwind()]
 });
