261d57c2133ae86cf24b44084934291b3a051b93
ojolowoblue <ojolowoblue@gmail.com>
2025-10-22 12:42:43 +0100

    refactor: Update personal branding in config with new title and enhanced description for clarity

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Ojolowo Boluwatife | Senior Frontend Engineer";
+export const SITE_TITLE = "Ojolowo Boluwatife | Senior Software Engineer";
 export const SITE_DESCRIPTION =
-  "With over half a decade of experience, Boluwatife is an experienced software engineer who builds engaging and accessible digital experiences.";
+  "Ojolowo Boluwatife is a Software Engineer dedicated to delivering high-quality work that not only meets expectations but exceeds them. With 6 years of experience and a strong focus on detail, innovation, and efficiency, I bring value by aligning my expertise with business goals to create meaningful results.";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
